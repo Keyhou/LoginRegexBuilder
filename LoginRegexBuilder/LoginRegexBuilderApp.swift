55b383ce2dd41ba4717676359d3bd104742e9295
Keyhou <31210145+Keyhou@users.noreply.github.com>
2023-02-27 10:01:10 +0100

    Initial Commit

---
+++ LoginRegexBuilder/LoginRegexBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginRegexBuilderApp.swift
+//  LoginRegexBuilder
+//
+//  Created by Keyhou on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginRegexBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
